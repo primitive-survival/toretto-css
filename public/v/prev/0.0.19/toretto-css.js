6759446d80a2b0b75428ebd2876946b4e93dbfa2
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2020-12-19 17:11:34 +0100

    v0.0.19

---
+++ public/v/prev/0.0.19/toretto-css.js
@@ -0,0 +1,171 @@
+
+import * as funcs from './toretto-funcs.js'
+const _runtime = {
+    options: {
+        prefix: null
+    },
+    generated: {}
+}
+const sizes = {
+    '@md': '(min-width: 50rem) and (max-width: 100rem)',
+    '@lg': '(min-width: 100rem)',
+    '^md': '(min-width: 50rem)',
+    '^lg': '(min-width: 100rem)',
+}
+export const components = {
+    Card: (props) => `<div>i'm card component</div>`
+}
+/** Chek If Tagname Is Standart Or Custom */
+export const isStandartTag = t => document.createElement(t) + '' !== '[object HTMLUnknownElement]'
+/** EscapeClassName
+ * 
+ */
+export const escapeClassName = cn => {
+    const p = cn.split(':')
+    const pseudo = p[1] ? ':' + p.slice(1).join(':') : ''
+    return cn.replace(/([()%@#:.^/]{1})/gi, '\\$1') + pseudo
+}
+/** Parse Class Name. 
+ *  
+ *  func[-arg1-arg2-...argX][^|@md|^|@lg][:hover|:after|:before]
+ */
+export const parseClassName = cn => {
+    if (_runtime.options.prefix)
+        cn = cn.replace(`${_runtime.options.prefix}_`, '')
+
+    const t = cn.includes('^')
+    const p = cn.split(':')
+    const s = t ? p[0].split('^') : p[0].split('@')
+    const a = s[0].split('-')
+    return {
+        func: a[0],
+        args: a.slice(1),
+        size: s[1] && (t ? '^' + s[1] : '@' + s[1]),
+        pseudo: p.slice(1)
+    }
+}
+
+/** Create Style Based On Class Name. 
+ * 
+ * */
+export const createStyle = cn => {
+
+    const tokens = parseClassName(cn.trim())
+    const rawStyle = (funcs[tokens.func] || funcs.fallback)(
+        tokens.func,
+        ...tokens.args
+    )
+    const escapedName = escapeClassName(cn)
+    let style = ''
+
+    if (tokens.size && sizes[tokens.size])
+        style = `@media screen and ${sizes[tokens.size]} { 
+            .${escapedName} {
+                ${rawStyle}
+            }
+        }`
+    else if (tokens.size) 
+        style = `.${tokens.size.substr(1)} .${escapedName} {
+            ${rawStyle}
+        }`
+    else
+        style = `.${escapedName} {
+            ${rawStyle}
+        }`
+    
+    return style
+}
+
+/** Generate Style From Nodes Added After Mutation. 
+ *  
+ */
+const _walkNodesCache = {
+    ids: {},
+
+}
+export function walkNodes(nodes) {
+    const styles = []
+
+    for (let node of nodes) {
+        if (components[node.tagName]) {
+            const props = {
+                children: node.innerHTML
+            }
+            for (let attr of node.attributes)
+                props[attr.name] = attr.value
+   
+            const _el = document.createElement('div')
+            _el.innerHTML = components[node.tagName](props) 
+
+            walkNodes(_el.querySelectorAll('*'))
+
+            node
+                .parentNode
+                .replaceChild(_el.firstChild, node)
+        }
+
+        if (!node.className)
+            continue
+
+        for (let cn of node.className.split(' ')) {
+            if (_runtime.generated[cn])
+                continue
+
+            _runtime.generated[cn] = true
+
+            const el = document.createElement('style')
+            el.innerHTML = createStyle(cn)
+            el.id = cn
+            document
+                .head
+                .appendChild(el)
+            styles.push(el)
+        }
+    }
+    return styles
+}
+
+/** Go. 
+ * 
+ */
+export const runToretto = (options = {}) => {
+    _runtime.options = {
+        ..._runtime.options,
+        ...options
+    }
+    let d = null
+    try {
+        d = document
+    } catch (e) {
+        /** Running at the server.
+         **/
+        return
+    }
+    d.querySelectorAll('template').forEach(template => {
+        template.id = template.id.toLowerCase()
+        components[template.id.toUpperCase()] = (props) => {
+            let htm = ''
+            eval(`htm=\`${template.innerHTML.trim()}\``)
+            return htm
+        }
+    })
+    const cb = function (mutationsList) {
+        for (let m of mutationsList) {
+            walkNodes(m.addedNodes)
+        }
+    }
+    const o = new MutationObserver(cb)
+    o.observe(
+        d.body,
+        {
+            attributes: true,
+            childList: true,
+            subtree: true
+        }
+    )
+    /** Generate Styles For Initial Html 
+     * */
+    walkNodes(d.body.querySelectorAll('*'))
+
+    d.body.style.visibility = 'visible'
+}
